83b285d86f9b8bb515dfb586f0c46bbd3c7aa164
nqd182 <nguyendung180203@gmail.com>
2025-05-05 16:04:41 +0700

    test

--- TechSavvy/obj/Debug/net9.0/TechSavvy.AssemblyInfo.cs
+++ TechSavvy/obj/Debug/net9.0/TechSavvy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechSavvy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d16cdfa2520cc57b9c92b9881d34748dbaffe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechSavvy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechSavvy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
